bc5185a5c10b4fbb24b0268b3939061c88d34e8e
rhydar <email.rhydar@gmail.com>
2015-10-25 17:33:47 +0200

    Update scales

--- R/metocean.R
+++ R/metocean.R
@@ -257,15 +257,19 @@ plotScatter <- function(input_df,col_index = c(2,3,4),labels,...){
   t3 <- scatterPlot(input_df,x=Tp,y=Dp,method = "hexbin",xbins=100,cols = "jet",aspect=1,hemisphere="southern")#,type="season")
 
   t1$plot$xlab <- list(label = labels[1],cex = 0.6,scales=list(cex=0.2))
-  #t1$plot$x.scales = list(cex=0.2)
   t1$plot$ylab <- list(label = labels[2],cex = 0.6,scales=list(cex=0.2))
+  t1$plot$x.scales$cex = c(0.6,0.6)
+  t1$plot$y.scales$cex = c(0.6,0.6)
 
   t2$plot$xlab <- list(label = labels[1],cex = 0.6,scales=list(cex=0.2))
   t2$plot$ylab <- list(label = labels[3],cex = 0.6,scales=list(cex=0.2))
+  t2$plot$x.scales$cex = c(0.6,0.6)
+  t2$plot$y.scales$cex = c(0.6,0.6)
 
   t3$plot$xlab <- list(label = labels[2],cex = 0.6,scales=list(cex=0.2))
   t3$plot$ylab <- list(label = labels[3],cex = 0.6,scales=list(cex=0.2))
-
+  t3$plot$x.scales$cex = c(0.6,0.6)
+  t3$plot$y.scales$cex = c(0.6,0.6)
   scatterPlots <- list(t1,t2,t3)
   return(scatterPlots)
 }
